b091b0ffea2cc871fae49573ccd7257a1347d92f
iracic82 <iracic82@gmail.com>
2025-06-04 19:21:03 +0200

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,9 +1,31 @@
-output "dc_public_ips" {
-  description = "Public IP addresses of the Domain Controllers"
-  value       = [aws_instance.dc1.public_ip, aws_instance.dc2.public_ip]
+output "domain_controllers" {
+  description = "Public IPs of the deployed Windows Server Domain Controllers (RDP enabled)"
+  value = {
+    dc1 = aws_instance.dc1.public_ip
+    dc2 = aws_instance.dc2.public_ip
+  }
 }
 
-output "private_key_path" {
-  description = "Path to the private key for RDP access"
-  value       = local_sensitive_file.private_key.filename
+output "rdp_connection_instructions" {
+  description = "How to connect to your DCs using RDP"
+  value = <<EOT
+Download the private key from:
+  ${local_sensitive_file.private_key.filename}
+
+Use the following commands (from your local machine):
+
+  # For DC1
+  ssh -i ./instruqt-dc-key.pem Administrator@${aws_instance.dc1.public_ip}
+
+  # For DC2
+  ssh -i ./instruqt-dc-key.pem Administrator@${aws_instance.dc2.public_ip}
+
+OR
+
+  Open Windows Remote Desktop (mstsc.exe) and connect to:
+    ${aws_instance.dc1.public_ip}  or  ${aws_instance.dc2.public_ip}
+
+  Username: Administrator
+  Password: (get from AWS Console: select instance → Connect → Get Password, using your private key)
+EOT
 }

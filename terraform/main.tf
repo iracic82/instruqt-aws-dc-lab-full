dd6b600fe71a9852dd2c509eb27edeee35f0ec54
iracic82 <iracic82@gmail.com>
2025-06-04 17:55:26 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.20.0"
-    }
-  }
-  required_version = ">= 1.3.0"
-}
-
 
 data "aws_availability_zones" "available" {}
 

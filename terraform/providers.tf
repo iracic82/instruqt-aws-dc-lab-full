b1cfaade1bd240d245a65ad247abe7f14938cf71
iracic82 <iracic82@gmail.com>
2025-06-04 17:54:55 +0200

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,7 +1,24 @@
+# initiate required Providers
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.20.0"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.90.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0" # or latest stable
+  }
+ }
+}
+
 provider "aws" {
   region     = var.aws_region
-  access_key = var.Access_Key_AWS
-  secret_key = var.Access_Secret_AWS
+  alias      = "eu-central-1"
 }
 
 data "aws_availability_zones" "available" {}

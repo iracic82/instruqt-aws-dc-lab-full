ac265f30caa1dde3314deedf2ad038b1adbb685f
iracic82 <iracic82@gmail.com>
2025-06-09 20:50:35 +0200

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -29,3 +29,27 @@ OR
   Password: (retrieve from AWS Console → Connect → Get Password using your key)
 EOT
 }
+
+output "infoblox_vnios_public_ips" {
+  description = "Public IPs of Infoblox vNIOS GM and GMC"
+  value = {
+    gm  = aws_eip.gm_eip.public_ip
+    gmc = aws_eip.gmc_eip.public_ip
+  }
+}
+
+output "infoblox_ui_access" {
+  description = "Access Infoblox Grid Manager and Grid Member Candidate via HTTPS UI"
+  value = <<EOT
+Grid Master (GM):
+  https://${aws_eip.gm_eip.public_ip}
+
+Grid Member Candidate (GMC):
+  https://${aws_eip.gmc_eip.public_ip}
+
+Default UI login:
+  Username: admin
+  Password: (value of var.infoblox_admin_password)
+EOT
+}
+

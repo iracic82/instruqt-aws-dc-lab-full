fd112a112d2b66eb42139626b66817482392477b
iracic82 <iracic82@gmail.com>
2025-06-10 16:55:07 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -99,6 +99,18 @@ resource "aws_security_group" "rdp_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   # AD required ports (internal only)
   dynamic "ingress" {
